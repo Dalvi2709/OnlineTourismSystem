c6cb2aa6b6c440e5d919c4bbae69b3e6c6de2895
Roshan Dalvi <roshandalvi16@gmail.com>
2025-09-26 12:14:22 +0530

    package fetch done

--- TourismWebApplication/TourismWebApplication/package.aspx.cs
+++ TourismWebApplication/TourismWebApplication/package.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +13,29 @@ namespace TourismWebApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindPackages();
+            }
+        }
+
+        private void BindPackages()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["MyDbConn"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string query = @"SELECT PackageID, ImageUrl, Location,
+                                 DATEDIFF(DAY, StartDate, EndDate) AS DurationDays,Title,
+                                 Price, Description, AvailableSlots
+                                 FROM Packages";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                rptPackages.DataSource = rdr;
+                rptPackages.DataBind();
+            }
 
         }
     }
